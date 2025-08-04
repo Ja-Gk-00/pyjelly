871d978c788c70b9f72892d9b238ab7cb526a613
JA-GK-00 <aaf6@o2.pl>
2025-08-04 17:59:22 +0200

    Test commit 2

---
+++ pyjelly/__init__.py
@@ -0,0 +1,8 @@
+try:
+    from ._version import __version__
+except ImportError:
+    import importlib.metadata
+    try:
+        __version__ = importlib.metadata.version(__name__)
+    except importlib.metadata.PackageNotFoundError:
+        __version__ = "0.0.0"
